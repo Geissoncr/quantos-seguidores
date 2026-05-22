dd2c22c9c1b42d98919aad365597ca6bff4a1cc4
Geissoncr <geissoncr@gmail.com>
2026-05-21 23:30:06 -0300

    Salvar Temperaturas

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import os
-from tabulate import tabulate
+import requests
 
-nome_arquivo = "seguidores.txt"
-seguidores = [["geissoncr", 30]]
+url='https://api.open-meteo.com/v1/forecast?latitude=-23.48&longitude=-46.68&hourly=apparent_temperature&current=temperature_2m&timezone=America%2FSao_Paulo&forecast_days=1'
+nome_arquivo = "temperaturas.txt"
 
-# 1. Verifica se o arquivo NÃO existe ou se está totalmente vazio (0 bytes)
+temperatura_hoje = requests.get(url).json()['current']
+data = temperatura_hoje['time']
+temperatura_agora = temperatura_hoje['temperature_2m']
+#seguidores = [["geissoncr", 30]]
+#nome_arquivo = "seguidores.txt"
+#usuario = "geissoncr"
+#qtde = 30
+
 arquivo_vazio = not os.path.exists(nome_arquivo) or os.path.getsize(nome_arquivo) == 0
 
-# 2. Abre o arquivo no modo "a" (append) para sempre acrescentar no final
 with open(nome_arquivo, "a", encoding="utf-8") as arquivo:
     if arquivo_vazio:
-        # Se estiver vazio, gera a tabela COMPLETA com o cabeçalho
-        tabela = tabulate(seguidores, headers=["usuario", "qtde Seguidores"])
-        print(tabela, file=arquivo)
-    else:
-        # Se NÃO estiver vazio, gera APENAS as linhas de dados, sem o cabeçalho
-        # O parâmetro tablefmt="plain" ajuda a manter o alinhamento simples
-        tabela_sem_header = tabulate(seguidores, tablefmt="plain")
-        print(tabela_sem_header, file=arquivo)
+        # Cria um cabeçalho simples separado por tabulação (\t)
+        arquivo.write("data\t\ttemperaturaAgora\n")
+        arquivo.write("-" * 30 + "\n")
+
+    # Acrescenta o registro formatado
+    arquivo.write(f"{data:<15}\t{temperatura_agora}\n")
